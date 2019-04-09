6978584d158765b864948b50393796a2292a5eb4
Nathaniel Tomtishen <tomtisnj@miamioh.edu>
2019-04-09 01:31:00 -0400

    Cleaning up item page

--- routes/items.js
+++ routes/items.js
@@ -3,8 +3,6 @@ var router = express.Router();
 const db = require('../db');
 const bcrypt = require('bcrypt');
 
-
-/* GET users listing. */
 router.get('/:itemID', async (req, res) => {
     id = req.params.itemID;
     query = "SELECT id AS \"ID\", name AS \"Name\", descrip AS \"Description\", developer AS \"Developer\", platform AS \"Platform\", platformv AS \"Version\", price AS \"Price\" \
