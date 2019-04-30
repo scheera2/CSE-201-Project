13ed20197225a7cf039ddf45bca26795d2103005
Nathaniel Tomtishen <tomtisnj@miamioh.edu>
2019-04-29 21:44:32 -0400

    Added delete capability, doesn't check if user is mod or admin however

--- routes/items.js
+++ routes/items.js
@@ -16,33 +16,42 @@ router.get('/:itemID', async (req, res) => {
 
 
 router.post('/:itemID', async (req, res) => {
-    const errors = [];
   
-    if (!req.body.comment) {
-      errors.push('Comment can not be empty');
-    }
-  
-    if (!errors.length) {
-      id = req.params.itemID;
-      date = new Date().getTime();
-      user = req.session.user.username;
+    if (typeof(req.body.deleteComment) == "undefined"){
+      const errors = [];
 
-      const queryAdd = '\
-          INSERT INTO comments(itemID, date, commentText, userName) \
-          VALUES (\'' + id + '\',\'' + date + '\',\'' + req.body.comment + '\',\'' + user + '\')';        
-          console.log(queryAdd);
-        await db.query(queryAdd);
-      res.redirect('/items/' + id);
-    } else {
-      id = req.params.itemID;
-      query = "SELECT id AS \"ID\", name AS \"Name\", descrip AS \"Description\", developer AS \"Developer\", platform AS \"Platform\", platformv AS \"Version\", price AS \"Price\" \
-      FROM appTest WHERE id=\' " + id + "\'; "
-      const result = await db.query(query);
-      query2 = "SELECT itemID AS \"itemID\", date AS \"Date\", commentText AS \"commentText\", userName AS \"userName\" \
-      FROM comments WHERE itemID=\' " + id + "\'; "
-      const result2 = await db.query(query2);
-      res.render('item', {rows: result.rows, commentInfo: result2.rows, user: req.session.user, errors: errors});
+      if (!req.body.comment) {
+        errors.push('Comment can not be empty');
+      }
+    
+      if (!errors.length) {
+        id = req.params.itemID;
+        date = new Date().getTime();
+        user = req.session.user.username;
+  
+        const queryAdd = '\
+            INSERT INTO comments(itemID, date, commentText, userName) \
+            VALUES (\'' + id + '\',\'' + date + '\',\'' + req.body.comment + '\',\'' + user + '\')';        
+            console.log(queryAdd);
+          await db.query(queryAdd);
+        res.redirect('/items/' + id);
+      } else {
+        id = req.params.itemID;
+        query = "SELECT id AS \"ID\", name AS \"Name\", descrip AS \"Description\", developer AS \"Developer\", platform AS \"Platform\", platformv AS \"Version\", price AS \"Price\" \
+        FROM appTest WHERE id=\' " + id + "\'; "
+        const result = await db.query(query);
+        query2 = "SELECT itemID AS \"itemID\", date AS \"Date\", commentText AS \"commentText\", userName AS \"userName\" \
+        FROM comments WHERE itemID=\' " + id + "\'; "
+        const result2 = await db.query(query2);
+        res.render('item', {rows: result.rows, commentInfo: result2.rows, user: req.session.user, errors: errors});
+      }
+    }else{
+      const queryDel = 'DELETE FROM comments WHERE date=\'' + req.body.deleteComment + '\' and itemID=\'' + req.params.itemID + "\'; "
+      console.log(queryDel);
+      await db.query(queryDel);
+      res.redirect('/items/' + req.params.itemID);
     }
+
   });
   
 
