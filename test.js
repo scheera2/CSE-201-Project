8f9301121a9d81e9f43ea044b95395b04f25f905
podgurja <podgurja@miamioh.edu>
2019-05-09 22:46:13 -0400

    FINALLY GOT TESTS WORKING FOR ROUTERS

--- test.js
+++ test.js
@@ -1,50 +1,50 @@
 let req = {
-    session: {
-        user: {
-            id: 1,
-            username: "test",
-            email: "test",
-            password: "$2b$10$BK5qiLjaSv.LUQUvZBS4aeYWGM4VfaItxygV8V6mWBMRfEiFbdxKS",
-            mod: null,
-            admin: null
-        },
-        adminUser: {
-          id: 2,
-          username: "asdf",
-          email: "asdf",
-          password: "$2b$10$BK5qiLjaSv.LUQUvZBS4aeYWGM4VfaItxygV8V6mWBMRfEiFbdxKS",
-          mod: null,
-          admin: 1
-      },
-      modUser: {
-        id: 3,
-        username: "asdf",
-        email: "asdf",
-        password: "$2b$10$BK5qiLjaSv.LUQUvZBS4aeYWGM4VfaItxygV8V6mWBMRfEiFbdxKS",
-        mod: 1,
-        admin: null
-      }
+  session: {
+    user: {
+      id: 1,
+      username: "test",
+      email: "test",
+      password: "$2b$10$BK5qiLjaSv.LUQUvZBS4aeYWGM4VfaItxygV8V6mWBMRfEiFbdxKS",
+      mod: null,
+      admin: null
     },
+    adminUser: {
+      id: 2,
+      username: "asdf",
+      email: "asdf",
+      password: "$2b$10$BK5qiLjaSv.LUQUvZBS4aeYWGM4VfaItxygV8V6mWBMRfEiFbdxKS",
+      mod: null,
+      admin: 1
+    },
+    modUser: {
+      id: 3,
+      username: "asdf",
+      email: "asdf",
+      password: "$2b$10$BK5qiLjaSv.LUQUvZBS4aeYWGM4VfaItxygV8V6mWBMRfEiFbdxKS",
+      mod: 1,
+      admin: null
+    }
+  },
 };
 
 var assert = require('assert');
 var db = require('./db.js');
 
-describe('UserHierarchy', function() {
-  describe('Admin', function() {
-    it('Should return 1 when user is an admin, null for mod status', function(){
+describe('UserHierarchy', function () {
+  describe('Admin', function () {
+    it('Should return 1 when user is an admin, null for mod status', function () {
       assert.equal(1, req.session.adminUser.admin);
       assert.equal(null, req.session.adminUser.mod);
     });
   });
-  describe('Moderator', function() {
-    it('Should return 1 for moderator status, null for admin status', function(){
+  describe('Moderator', function () {
+    it('Should return 1 for moderator status, null for admin status', function () {
       assert.equal(null, req.session.modUser.admin);
       assert.equal(1, req.session.modUser.mod);
     });
   });
-  describe('User', function() {
-    it('Should return null for moderator status, null for admin status', function(){
+  describe('User', function () {
+    it('Should return null for moderator status, null for admin status', function () {
       assert.equal(null, req.session.user.admin);
       assert.equal(null, req.session.user.mod);
     });
@@ -52,20 +52,69 @@ describe('UserHierarchy', function() {
 });
 
 
-describe('Account Information', function() {
+describe('Account Information', function () {
 
-  describe('User', function() {
-    it('Password should be hashed', function(){
+  describe('User', function () {
+    it('Password should be hashed', function () {
       assert.equal("$2b$10$BK5qiLjaSv.LUQUvZBS4aeYWGM4VfaItxygV8V6mWBMRfEiFbdxKS", req.session.user.password);
-      });
+    });
   });
-  describe('Database', function() {
-    it('Query DB', function(){
-      
+  describe('Database', function () {
+    it('Query DB', function () {
+
       const selectQuery = 'SELECT * FROM appUsers WHERE username = $1';
       const selectResult = db.query(selectQuery, "test");
       console.log(selectResult.rows);
-      });
+    });
   });
 
 });
+
+var express = require('express');
+var request = require('supertest');
+var router = express.Router();
+var app = express();
+// app is the express context from the server
+describe('GET /users/login', function () {
+  it('test api', function (done) {
+    request(app)
+      .get('/login')
+      .end(function (err, res) {
+        assert.equal(404, res.statusCode);
+        done();
+      });
+  });
+  it('test api', function (done) {
+    request(app)
+      .get('/register')
+      .end(function (err, res) {
+        assert.equal(404, res.statusCode);
+        done();
+      });
+  });
+  it('test api', function (done) {
+    request(app)
+      .get('/add')
+      .end(function (err, res) {
+        assert.equal(404, res.statusCode);
+        done();
+      });
+  });
+  it('test api', function (done) {
+    request(app)
+      .get('/change-password')
+      .end(function (err, res) {
+        assert.equal(404, res.statusCode);
+        done();
+      });
+  });
+  it('test api', function (done) {
+    request(app)
+      .get('/pending')
+      .end(function (err, res) {
+        assert.equal(404, res.statusCode);
+        done();
+      });
+  });
+});
+module.exports = router;
